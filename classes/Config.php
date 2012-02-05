e65d5020738e2d9e65086b96d27adf39ddecc072
Dave Widmer <dave@davewidmer.net>
2012-02-04 21:00:15 -0500

    Creating a configuration class.

---
+++ classes/Config.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Nest;
+
+/**
+ * Configuration for the Nest library.
+ *
+ * @package  Nest
+ * @author   Dave Widmer <dave@davewidmer.net>
+ */
+class Config
+{
+	/**
+	 * @var string  The base_url for the site
+	 */
+	public $base_url = "/";
+
+	/**
+	 * @var string  The name of the renderer.
+	 */
+	public $renderer = "Markdown";
+
+	/**
+	 * @var string  The extension to look for
+	 */
+	public $extension = ".md";
+
+	/**
+	 * Creates a new configuration object
+	 *
+	 * @param array $data The data for the configuration.
+	 */
+	public function __construct(array $data)
+	{
+		foreach ($data as $key => $value)
+		{
+			$this->{$key} = $value;
+		}
+	}
+
+}
