b173ab419a6244a986c91f4201278b3383561adb
Dave Widmer <dave@davewidmer.net>
2012-02-04 20:57:14 -0500

    Removing the autoload of Markdown, this is the renderers responsibility.

--- bootstrap.php
+++ bootstrap.php
@@ -5,6 +5,3 @@ include "classes".DIRECTORY_SEPARATOR."Autoloader.php";
 
 $autoload = new \Nest\Autoloader;
 $autoload->register();
-
-// Load up the Markdown renderer
-include "vendor".DIRECTORY_SEPARATOR."markdown".DIRECTORY_SEPARATOR."markdown.php";
