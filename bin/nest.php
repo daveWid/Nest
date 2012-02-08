0f771b2c8f8facd736877691f6d9e3ea44d7f8f5
Dave Widmer <dave@davewidmer.net>
2012-02-07 21:00:18 -0500

    Moving the nest cli script to use some realpath magic so the library can live anywhere.

--- bin/nest.php
+++ bin/nest.php
@@ -2,7 +2,7 @@
 <?php
 
 // Bootstrap
-include "Nest/bootstrap.php";
+include realpath(dirname(__FILE__).DIRECTORY_SEPARATOR."..").DIRECTORY_SEPARATOR."bootstrap.php";
 
 // CLI
 $cli = new \Nest\CLI($argv);
