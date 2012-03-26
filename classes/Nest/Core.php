63e7f422cbe682a2cc9371e16eff8e21844f2ad6
Dave Widmer <dave@davewidmer.net>
2012-03-26 11:38:13 -0400

    Streamlining the core.

--- classes/Nest/Core.php
+++ classes/Nest/Core.php
@@ -13,133 +13,67 @@ namespace Nest;
 class Core
 {
 	/**
-	 * @var string The base url for all files on the site.
+	 * @var string The path to the document root for the project
 	 */
-	public static $base_url;
+	private $docroot;
 
 	/**
-	 * @var string The base path for the library.
+	 * @var array  A list of supported rendering engines
 	 */
-	public static $system_path = null;
-
-	/**
-	 * Gets a url for an asset.
-	 *
-	 * @param  string $path The url of the asset to find
-	 * @return string       The url with the base path prepended
-	 */
-	public static function url($path = "")
-	{
-		return static::$base_url.$path;
-	}
-
-	/**
-	 * Finds a file from the library file sytstem.
-	 *
-	 * @param  string $dir   The directory to look in
-	 * @param  string $file  The file to find
-	 * @param  string $ext   The file extension
-	 * @return string        The full file path
-	 */
-	public static function find_file($dir, $file, $ext = ".php")
-	{
-		$found = false;
-		$path = static::$system_path.$dir.DIRECTORY_SEPARATOR.$file.$ext;
-		if (is_file($path))
-		{
-			$found = $path;
-		}
-
-		return $found; 
-	}
-
-	/**
-	 * @var string The path to the public folder
-	 */
-	private $public_path;
-
-	/**
-	 * @var string The full path to all of the wiki files.
-	 */
-	private $wiki_path;
-
-	/**
-	 * @var \Nest\Config  The configuration object
-	 */
-	private $config;
-
-	/**
-	 * @var array  A list of supported renderers
-	 */
-	public $renderers = array(
-		'php' => "\\Nest\\Renderer\\PHP",
-		'md' => "\\Nest\\Renderer\\Markdown",
-		'markdown' => "\\Nest\\Renderer\\Markdown",
-		'textile' => "\\Nest\\Renderer\\Textile",
-		'mustache' => "\\Nest\\Renderer\\Mustache"
+	private $engines = array(
+		'php' => "\\Nest\\Engine\\PHP",
+		'md' => "\\Nest\\Engine\\Markdown",
+		'markdown' => "\\Nest\\Engine\\Markdown",
+		'textile' => "\\Nest\\Engine\\Textile",
+		'mustache' => "\\Nest\\Engine\\Mustache"
 	);
 
 	/**
-	 * @var string The name of the file that was requested to be loaded.
-	 */
-	private $file_path;
-
-	/**
-	 * Creates a new Core object
+	 * Creates a new Nest object. If a configuration array isn't passed in
+	 * then the system will try and load config.ini from the docroot.
 	 *
-	 * @param string       $path    The path to the public server folder
-	 * @param \Nest\Config $config  The site configuration
+	 * @param string $path    The path to the public server folder
 	 */
-	public function __construct($path, \Nest\Config $config)
+	public function __construct($path)
 	{
-		$this->public_path = rtrim($path, DIRECTORY_SEPARATOR).DIRECTORY_SEPARATOR;
-		$this->wiki_path = $this->public_path."_wiki".DIRECTORY_SEPARATOR;
-		$this->config = $config;
-
-		static::$base_url = $this->config->base_url;
-		static::$system_path = $this->config->system_path;
+		$this->docroot = rtrim($path, DIRECTORY_SEPARATOR).DIRECTORY_SEPARATOR;
 	}
 
 	/**
 	 * Runs the actions that builds the page.
 	 *
-	 * @param  string $file   The path to execute (null uses path_info)
-	 * @return string         The html to display.
+	 * @param  string $file    The path to execute (null uses path_info)
+	 * @param  string $layout  The path to the template file to use.
+	 * @return string          The html to display.
 	 */
-	public function execute($file = null)
-	{
-		$this->file_path = $this->locate_view($file) ?:
-			$this->get_default("error", ".md");
-
-		$ext = pathinfo($this->file_path, PATHINFO_EXTENSION);
-
-		$renderer = new $this->renderers[$ext];
-		$content = $renderer->render($this->file_path);
-
-		// Switch the renderer to php if it isnt already
-		if ($ext !== "php")
-		{
-			$renderer = new \Nest\Renderer\PHP;
-		}
-
-		return $renderer->render($this->get_default("layout"), array(
-			'content' => $content
-		));
-	}
-
-	/**
-	 * Locates a view that is used as the main content.
-	 *
-	 * @param  string $file  The filename of the view to find
-	 * @return mixed         The (string) filename OR (boolean) false
-	 */
-	private function locate_view($file)
+	public function execute($file = null, $layout = "layout")
 	{
 		if ($file === null)
 		{
 			$file = (isset($_SERVER['PATH_INFO'])) ? $_SERVER['PATH_INFO'] : "/" ;
 		}
 
+		// Get the page content first.
+		$page = $this->find_file($file);
+		$content = $this->render($page);
+
+		// Now mix in into the layout and return
+		$layout = $this->find_file($layout);
+		return $this->render($layout, array(
+			'content' => $content
+		));
+	}
+
+	/**
+	 * Locates a given file.
+	 *
+	 * @throws \Nest\Execption  Thrown when a page is not found
+	 *
+	 * @param  string $file  The filename of the view to find
+	 * @return mixed         The (string) filename OR (boolean) false
+	 */
+	private function find_file($file)
+	{
 		if (substr($file, -1) === "/")
 		{
 			$file .= "index.html";
@@ -147,11 +81,31 @@ class Core
 
 		$base = preg_replace("/.html$/", "", ltrim($file, "/"));
 
-		$exts = implode(",", array_keys($this->renderers));
-		$pattern = $this->wiki_path().$base.".{".$exts."}";
+		$exts = implode(",", array_keys($this->engines));
+		$pattern = $this->get_wiki_path().$base.".{".$exts."}";
 		$found = glob($pattern, GLOB_BRACE);
 
-		return (empty($found)) ? false : $found[0];
+		if (empty($found))
+		{
+			throw new \Nest\Exception("The {$file} page could not be found");
+		}
+
+		return $found[0];
+	}
+
+	/**
+	 * Takes a full server path and returns the rendered content.
+	 *
+	 * @param  string $path  The path to the resource
+	 * @param  array         Parameters to add to the rendering engine
+	 * @return string        Rendered HTML
+	 */
+	private function render($path, $params = array())
+	{
+		$ext = pathinfo($path, PATHINFO_EXTENSION);
+		$engine = new $this->engines[$ext];
+
+		return $engine->render($path, $params);
 	}
 
 	/**
@@ -159,9 +113,9 @@ class Core
 	 *
 	 * @return string
 	 */
-	public function docroot()
+	public function get_docroot()
 	{
-		return $this->public_path;
+		return $this->docroot;
 	}
 
 	/**
@@ -169,31 +123,9 @@ class Core
 	 *
 	 * @return string
 	 */
-	public function wiki_path()
+	public function get_wiki_path()
 	{
-		return $this->wiki_path;
-	}
-
-	/**
-	 * Gets the path for the default layout/error page.
-	 *
-	 * This looks in the _wiki directory first and if not found, grabs the file
-	 * from the system/views directory.
-	 *
-	 * @param  string $file  The filen ame to find
-	 * @param  string $ext   The file extension
-	 * @return string        The full path
-	 */
-	private function get_default($file, $ext = ".php")
-	{
-		$path = $this->wiki_path.$file.$ext;
-
-		if ( ! is_file($path))
-		{
-			$path = static::find_file("views", $file, $ext);
-		}
-
-		return $path;
+		return $this->docroot."_wiki".DIRECTORY_SEPARATOR;
 	}
 
 }
