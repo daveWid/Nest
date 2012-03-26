12ff613923fb2c83e81a15de1a363cd655672d48
Dave Widmer <dave@davewidmer.net>
2012-03-26 11:38:27 -0400

    Updating the rendering engines.

--- classes/Nest/Engine/Markdown.php
+++ classes/Nest/Engine/Markdown.php
@@ -2,6 +2,9 @@
 
 namespace Nest\Engine;
 
+// We need this for the MarkdownExtra_Parser class...
+include_once realpath(__DIR__.DIRECTORY_SEPARATOR."..".DIRECTORY_SEPARATOR."..".DIRECTORY_SEPARATOR."Markdown.php");
+
 /**
  * Markdown Renderer
  *
@@ -10,20 +13,6 @@ namespace Nest\Engine;
  */
 class Markdown extends \MarkdownExtra_Parser implements \Nest\Engine
 {
-	/**
-	 * Adds some extra flavor into the Markdown parser
-	 */
-	public function __construct()
-	{
-		// doImage is 10, add image url just before
-		$this->span_gamut['doImageURL'] = 9;
-
-		// doLink is 20, add base url just before
-		$this->span_gamut['doBaseURL'] = 19;
-
-		parent::MarkdownExtra_Parser();
-	}
-
 	/**
 	 * Renders the source into HTML
 	 *
@@ -34,34 +23,6 @@ class Markdown extends \MarkdownExtra_Parser implements \Nest\Engine
 	public function render($file, $data = array())
 	{
 		return $this->transform(file_get_contents($file));
-	}
-
-	/**
-	 * Add the current base url to all local links.
-	 *
-	 * URLs containing "://" are left untouched
-	 *
-	 * @param   string  $text  The text to check
-	 * @return  string
-	 */
-	public function doBaseURL($text)
-	{
-		$pattern = '~(?<!!)(\[.+?\]\()(?!\w++://)(?!#)(\S*(?:\s*+".+?")?\))~';
-		return preg_replace($pattern, '$1'.\Nest\Core::$base_url.'$2', $text);
-	}
-
-	/**
-	 * Add the current base url to all local images.
-	 *
-	 * URLs containing "://" are left untouched
-	 *
-	 * @param   string  $text  The text to check
-	 * @return  string
-	 */
-	public function doImageURL($text)
-	{
-		$pattern = '~(!\[.+?\]\()(?!\w++://)(\S*(?:\s*+".+?")?\))~';
-		return preg_replace($pattern, '$1'.\Nest\Core::$base_url.'$2', $text);
 	}
 
 }
--- classes/Nest/Engine/Textile.php
+++ classes/Nest/Engine/Textile.php
@@ -12,30 +12,6 @@ namespace Nest\Engine;
 class Textile extends \Textile implements \Nest\Engine
 {
 	/**
-	 * Creates a new Textile renderer.
-	 */
-	public function __construct()
-	{
-		parent::Textile();
-	}
-
-	/**
-	 * Adds the base url to the pages if the url doesn't start with / or http(s)
-	 *
-	 * @param  string $text  The url to "shelve"
-	 * @return string        The reference to inject into the text
-	 */
-	public function shelveURL($text)
-	{
-		if (preg_match("/^((\w+:\/\/)|\/)/", $text) === 0)
-		{
-			$text = \Nest\Core::$base_url.$text;
-		}
-
-		return parent::shelveURL($text);
-	}
-
-	/**
 	 * Renders the source into HTML
 	 *
 	 * @param  string $file  The path to the file to render
