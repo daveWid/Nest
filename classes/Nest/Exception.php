bbee810ba5396307d28bb68e04566158dc0d4107
Dave Widmer <dave@davewidmer.net>
2012-03-26 11:19:30 -0400

    Adding an exception class.

---
+++ classes/Nest/Exception.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Nest;
+
+/**
+ * Exception handling for the Nest library.
+ *
+ * @package  Nest
+ * @author   Dave Widmer <dave@davewidmer.net>
+ */
+class Exception extends \Exception{}
